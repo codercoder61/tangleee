807a8538d826a98e1b89ed84650cb3d899cad5c0
codercoder61 <mohamedknoud@gmail.com>
2025-12-13 22:34:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import axios from 'axios';
 import { useFirebaseNotifications } from "./useFirebaseNotifications";
 
 function App() {
-	useFirebaseNotifications();
   return (
     <Router>
       <Routes>
